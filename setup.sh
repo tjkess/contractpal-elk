7b0eca2a2f97a303093d04ce2c31a09dc15fbb2e
TJ Kessinger <40049575+tjkess@users.noreply.github.com>
2020-01-08 09:15:59 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -4,4 +4,15 @@ sudo yum install -y java-1.8.0-openjdk
 
 sudo rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
 
+touch /etc/yum.repos.d/logstash.repo
+
+echo '[logstash-6.x]
+name=Elastic repository for 6.x packages 
+baseurl=https://artifacts.elastic.co/packages/6.x/yum 
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+autorefresh=1
+type=rpm-md' > /etc/yum.repos.d/logstash.repo
+
 
