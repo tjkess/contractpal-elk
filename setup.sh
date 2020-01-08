c26718bea826778ea763c6a8d4892e7a51041c73
TJ Kessinger <40049575+tjkess@users.noreply.github.com>
2020-01-08 09:17:50 -0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -15,4 +15,25 @@ enabled=1
 autorefresh=1
 type=rpm-md' > /etc/yum.repos.d/logstash.repo
 
+sudo yum install -y logstash
 
+touch /etc/logstash/conf.d/logstash.conf
+
+echo 'input{
+    s3 {
+        bucket => "elk-test-bucke"
+        access_key_id => "key"
+        secret_access_key => "secret"
+        region => "us-west-2"
+        interval => "10"
+    }
+}
+output {
+    elasticsearch {
+        hosts => ["elasticsearch/endpoint:443"]
+        ssl => "true"
+       # index => "production-logs-%{+YYYY.MM.dd}"
+    }
+}' > /etc/logstash/conf.d/logstash.conf
+
+sudo systemctl start logstash
